138612f9fa4776a12249f0ff83d659859ff508e0
davide.patrizi <d.patrizi@nextarconsulting.com>
2015-04-30 21:31:16 +0200

    fist add for project

---
+++ Augury/app/src/main/java/augury/it/augury/Alarm/ManageAlarm.java
@@ -0,0 +1,17 @@
+package augury.it.augury.Alarm;
+
+import augury.it.augury.Model.Friend;
+
+/**
+ * Created by Bobaldo on 30/04/2015.
+ */
+public class ManageAlarm {
+    public void setAlarm(Friend friend) {
+    }
+
+    public void removeAlarm(Friend friend) {
+    }
+
+    public void restoreAlarmAfterBoot() {
+    }
+}
---
+++ Augury/app/src/main/java/augury/it/augury/Model/Friend.java
@@ -0,0 +1,11 @@
+package augury.it.augury.Model;
+
+/**
+ * Created by Bobaldo on 30/04/2015.
+ */
+public class Friend {
+
+    public int id;
+
+
+}
