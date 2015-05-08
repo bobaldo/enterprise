3bec904ba89ef7c8555fecaf501ab44bf8be2285
davide.patrizi <d.patrizi@nextarconsulting.com>
2015-05-08 15:54:43 +0200

    add sctructure

---
+++ Augury/app/src/main/java/augury/it/augury/Storage/ManageParse.java
@@ -0,0 +1,30 @@
+package augury.it.augury.Storage;
+
+import java.util.List;
+
+import augury.it.augury.Model.Friend;
+
+/**
+ * Created by Bobaldo on 30/04/2015.
+ */
+public class ManageParse {
+    public void autenticate(String username, String password) {
+    }
+
+    public void insert(Friend friend) {
+    }
+
+    public void update(Friend friend) {
+    }
+
+    public void delete(int id) {
+    }
+
+    public Friend get(int id) {
+        return null;
+    }
+
+    public List<Friend> get() {
+        return null;
+    }
+}
