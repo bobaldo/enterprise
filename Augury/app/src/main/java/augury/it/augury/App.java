826548614d7bef959e7fa513261761df834e0399
davide.patrizi <d.patrizi@nextarconsulting.com>
2015-05-08 15:27:10 +0200

    add App class

---
+++ Augury/app/src/main/java/augury/it/augury/App.java
@@ -0,0 +1,25 @@
+package augury.it.augury;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseInstallation;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mursi_000 on 02/05/2015.
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
