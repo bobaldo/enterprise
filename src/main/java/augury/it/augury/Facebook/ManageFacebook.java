997bada6f396482fcdebfa4e6f2887b30ed700c6
davide.patrizi <d.patrizi@nextarconsulting.com>
2015-05-08 15:02:16 +0200

    try clear solution

---
+++ src/main/java/augury/it/augury/Facebook/ManageFacebook.java
@@ -0,0 +1,18 @@
+package augury.it.augury.Facebook;
+
+import java.util.List;
+
+import augury.it.augury.Model.Friend;
+
+
+/**
+ * Created by Bobaldo on 30/04/2015.
+ */
+public class ManageFacebook {
+    public void autenticate(String username, String Password) {
+    }
+
+    public List<Friend> getFriends() {
+        return null;
+    }
+}
